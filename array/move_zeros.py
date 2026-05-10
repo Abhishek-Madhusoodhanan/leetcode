bdc832a3225a77442251325654f8ef5811e250eb
Abhishek-Madhusoodhanan <abhishekmadhusoodhanan18@gmail.com>
2026-05-10 12:23:11 +0530

    move_zeros

---
+++ array/move_zeros.py
@@ -0,0 +1,31 @@
+'''Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.'''
+
+
+# Example 1:
+# Input: nums = [0,1,0,3,12]    
+# Output: [1,3,12,0,0]
+
+
+# Example 2:
+# Input: nums = [0]     
+# Output: [0]
+
+
+
+class Solution:
+    def movezeros(self,nums):
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left] = nums[right]
+                left += 1
+        for right in range(left, len(nums)):
+            nums[right] = 0
+
+# vs code update for running the code in the terminal
+
+if __name__ == "__main__":
+    nums = [0,1,0,3,12]
+    Solution().movezeros(nums)
+    print(nums)
