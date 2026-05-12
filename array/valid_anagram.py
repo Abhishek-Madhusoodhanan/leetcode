b96bc4d027a6b419d26568a55e78dbbb497f6caf
Abhishek-Madhusoodhanan <abhishekmadhusoodhanan18@gmail.com>
2026-05-12 14:17:05 +0530

    valid anagram

---
+++ array/valid_anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        seen = {}
+
+        for ch in s:
+            seen[ch] = seen.get(ch,0)+1
+
+        for ch in t:
+            if ch not in seen:
+                return False
+            seen[ch] -=1
+            if seen[ch] == 0:
+                del seen[ch]
+        return len(seen) ==0
